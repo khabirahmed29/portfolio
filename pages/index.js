fb30b620b4ba4dbec9904a313a989252696b9971
Khabir Ahmed <khabirahmed780@gmail.com>
2025-01-29 22:39:07 +0530

    abc

--- pages/index.js
+++ pages/index.js
@@ -136,7 +136,7 @@ export default function Home() {
             </div>
           </div> */}
         </section>
-        <section className="py-10">
+        {/* <section className="py-10">
           <div>
             <h3 className="text-3xl py-1 dark:text-white ">Portofolio</h3>
             <p className="text-md py-2 leading-8 text-gray-800 dark:text-gray-200">
@@ -208,7 +208,7 @@ export default function Home() {
               />
             </div>
           </div>
-        </section>
+        </section> */}
       </main>
     </div>
   );
